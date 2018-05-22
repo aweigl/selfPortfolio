6523c0d3bd4be762e3b6b4d70421bc60d34c8f64
Aaron Weigl <aaron.weigl@hotmail.com>
2018-05-22 23:14:53 +0200

    linting

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const express = require('express');
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
+app.use(express.static(`${__dirname}/public`));
 
-app.use(express.static(`${__dirname}/public`))
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/public/main.html`);
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/public/main.html`)
-})
-
-app.listen(port, () =>
-    console.log(`Listening on port ${port}`)
-);
+app.listen(port, () => console.log(`Listening on port ${port}`));
