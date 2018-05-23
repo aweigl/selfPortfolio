48a65c74a38e701d78f652664f53cbb2cb28e8d6
Aaron Weigl <aaron.weigl@hotmail.com>
2018-05-23 17:36:38 +0200

    css

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ function scrollToAnchor(aid) {
     var aTag = $("a[name='" + aid + "']");
     $('html, body').animate({
         scrollTop: aTag.offset().top
-    }, "slow")
+    }, "slow");
 }
 
 
