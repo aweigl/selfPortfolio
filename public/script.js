640233db86a7644f06824d63096658facba7b6f7
Aaron Weigl <aaron.weigl@hotmail.com>
2018-05-24 12:28:31 +0200

    images folder added

--- public/script.js
+++ public/script.js
@@ -1,31 +1,31 @@
-const scrollToAnchor = (aid) => {
+const scrollToAnchor = (aid, offset) => {
     let aTag = $(`a[name=${aid}]`);
     $(`html`).animate({
-        scrollTop: aTag.offset().top - 50
+        scrollTop: aTag.offset().top - offset
     }, "slow")
 }
 
 $('#aboutMe').click(() => {
-    scrollToAnchor('link2');
+    scrollToAnchor('link2', 100);
     console.log($('.aboutMe').position());
 
 })
 
 $('#projects').click(() => {
-    scrollToAnchor('link1');
+    scrollToAnchor('link1', 50);
     console.log($('.projects').position());
 })
 
 $('#skills').click(() => {
-    scrollToAnchor('skills')
+    scrollToAnchor('skills', 50)
     console.log($('.skills').position());
 })
 
 $('#contact').click(() => {
-    scrollToAnchor('link3')
+    scrollToAnchor('link3', 50)
     console.log($('.contact').position());
 })
 
 $('#textLink').click(() => {
-    scrollToAnchor('skills')
+    scrollToAnchor('skills', 50)
 })
