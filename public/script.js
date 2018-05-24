d36f0547d68aebc9786c54276f47c837988fd3c5
Aaron Weigl <aaron.weigl@hotmail.com>
2018-05-24 12:19:41 +0200

    about me

--- public/script.js
+++ public/script.js
@@ -6,26 +6,26 @@ const scrollToAnchor = (aid) => {
 }
 
 $('#aboutMe').click(() => {
-    $('#menu1').addClass('underlined');
     scrollToAnchor('link2');
     console.log($('.aboutMe').position());
 
 })
 
 $('#projects').click(() => {
-    $('#menu2').addClass('underlined');
     scrollToAnchor('link1');
     console.log($('.projects').position());
 })
 
 $('#skills').click(() => {
-    $('#menu3').addClass('underlined');
     scrollToAnchor('skills')
     console.log($('.skills').position());
 })
 
 $('#contact').click(() => {
-    $('#menu4').addClass('underlined');
     scrollToAnchor('link3')
     console.log($('.contact').position());
+})
+
+$('#textLink').click(() => {
+    scrollToAnchor('skills')
 })
