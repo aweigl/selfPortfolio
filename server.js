125b396d9b0dfddc70d5b91f0ffa1a14e96c71e1
Aaron Weigl <aaron.weigl@hotmail.com>
2018-06-07 11:19:44 +0200

    sitemap

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(`${__dirname}/public`));
 
-app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/public/main.html`);
+app.get("/sitemap", (req, res) => {
+    res.sendFile(`${__dirname}/sitemap.xml`);
 });
 
-
-app.get('google-analytics/sitemap', (req, res) => {
-    res.sendFile(`${__dirname}/sitemap.xml`);
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/public/main.html`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
